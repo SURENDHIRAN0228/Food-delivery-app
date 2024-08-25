7cfe258f37037af7ca482b55fb639f5530ad994c
Surendhar <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-08-25 11:03:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,3 @@
-
-/*global.foodData = require('./db')(function call(err, data, CatData) {
-  // console.log(data)
-  if(err) console.log(err);
-  global.foodData = data;
-  global.foodCategory = CatData;
-})*/
-
 const express = require('express')
 const app = express()
 const port = 5000
