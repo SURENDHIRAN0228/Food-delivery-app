9580917be824c9b807e6ab0632b1967213115f9c
Surendhar <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-08-25 12:59:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,39 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const cors = require('cors');
+app.use(cors());
+
+const http = require('http').Server(app)
+
+const mongoDB = require("./db")
+mongoDB.connectDb();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api/auth', require('./Routes/Auth'));
+
+http.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
+/*const express = require('express')
+const app = express()
+const port = 5000
+
 const mongoDB = require("./db")
 mongoDB.connectDb();
 
@@ -24,4 +57,4 @@ app.use('/api/auth', require('./Routes/Auth'));
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
-
+*/
