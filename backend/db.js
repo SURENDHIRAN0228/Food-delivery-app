3a67f55551634f963e09143f0c903ea24b0ddefc
Surendhar <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-08-25 11:35:12 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,38 @@
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://surendhar11091:WfDkKSWNuEJrWTVE@gofood.wv2ow.mongodb.net/?retryWrites=true&w=majority&appName=GoFood";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDb() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+module.exports= { connectDb }
+
+
+
+
+
+
+
+
+/*const mongoose = require("mongoose");
 
 const mondbUrl = "mongodb+srv://surendhar11091:WfDkKSWNuEJrWTVE@gofood.wv2ow.mongodb.net/?retryWrites=true&w=majority&appName=GoFood"
 //const mondbUrl = "mongodb://127.0.0.1:27017/GoFood"  WfDkKSWNuEJrWTVE
